5a8bb86a2a346adcac2c923c516968cd8856fb9a
Leonardo-Ximenes <cintra26102006@gmail.com>
2026-03-08 17:33:25 -0300

    Create Lista-respondida.py

---
+++ Banco-NoSQL/Introdução-Bancos-de-Dados-Não-Relacionais/Lista-respondida.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Define a URL do "World" ou qualquer site
+url = "https://1drv.ms/w/c/d8e40bb9f3d04344/IQD_yMKlI2k7T5SqFuL5gQiwAeqV1kxRLIhPVLRS11yT8D4?e=KWFVRH"
+
+# Abre no navegador padrão
+webbrowser.open(url)
